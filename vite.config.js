a35d14ccd0ebd51aadef8de01c93cf61fc83b9e8
patiencemanzen <hseal419@gmail.com>
2026-04-27 20:51:06 +0200

    feat: update Netlify configuration and add SPA routing for improved deployment

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,21 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
     rollupOptions: {
       output: {
         entryFileNames: '[name].js',
         chunkFileNames: '[name]-[hash].js',
         assetFileNames: '[name]-[hash][extname]'
       }
+    }
+  },
+  server: {
+    mimeTypes: {
+      'js': 'text/javascript',
+      'jsx': 'text/javascript'
     }
   }
 })

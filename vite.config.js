7a3eb290458ee0b18db3808dda129344faa4ebd1
patiencemanzen <hseal419@gmail.com>
2026-04-27 20:29:32 +0200

    feat: add build configuration and Netlify headers for JavaScript files

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash][extname]'
+      }
+    }
+  }
 })
